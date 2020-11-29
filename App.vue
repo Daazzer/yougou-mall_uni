70dd0e3876568f8dbf2faf03c145ee49264fdd27
Daazzer <Daazzer@github.com>
2020-11-29 19:59:43 +0800

    添加 box-sizing 样式

--- App.vue
+++ App.vue
@@ -1,17 +1,24 @@
 <script>
 export default {
-  onLaunch: function() {
-  },
-  onShow: function() {
-  },
-  onHide: function() {
-  }
+  onLaunch: function() {},
+  onShow: function() {},
+  onHide: function() {}
 }
 </script>
 
 <style lang="scss">
- @import "@/static/fonts/iconfont.css";
+@import "@/static/fonts/iconfont.css";
+
 .pt-100 {
   padding-top: 100rpx;
+}
+
+view,
+scroll-view,
+navigator,
+input,
+swiper,
+image {
+  box-sizing: border-box;
 }
 </style>
