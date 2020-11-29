0a5f8aae0a892d1ee0219608f5f3affb9b90c1be
Daazzer <Daazzer@github.com>
2020-11-29 15:50:58 +0800

    将通用方法注入到 vue 实例原型中

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import api from './utils/api'
+import { showErrorTips } from './utils'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+Vue.prototype.$api = api
+Vue.prototype.$showErrorTips = showErrorTips
 
 const app = new Vue({
     ...App
---
+++ utils/index.js
@@ -0,0 +1,20 @@
+export const getRect = selector => {
+  const p = new Promise(rv => {
+    wx.createSelectorQuery().select(selector).boundingClientRect(rect => {
+      rv(rect)
+    }).exec()
+  })
+
+  return p
+}
+/**
+ * 集中显示响应错误
+ * @param {string} msg 自定义的提示信息
+ * @param {Error} err 错误对象
+ */
+export const showErrorTips = (err, msg) => {
+  if (err.msg) {
+    msg = msg + '，' + err.msg
+  }
+  uni.showToast({ title: msg, icon: 'none' })
+}
