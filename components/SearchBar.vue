8778c7f1b973e17ea5fb5015fe89ae065588db30
Daazzer <Daazzer@github.com>
2020-11-29 15:28:34 +0800

    删除搜索栏组件目录，将搜索栏放到组件根目录

---
+++ components/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="search-bar">
+    <navigator class="search-bar__btn" url="/pages/search/index">搜索</navigator>
+  </view>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+.search-bar {
+  height: 100rpx;
+  padding: 10rpx 25rpx;
+  box-sizing: border-box;
+  background-color: #ea4350;
+  &.fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+  }
+  &__btn {
+    display: block;
+    height: 100%;
+    width: 100%;
+    border-radius: 5rpx;
+    font-size: 32rpx;
+    line-height: 80rpx;
+    text-align: center;
+    color: #767676;
+    background-color: #fff;
+  }
+}
+</style>
