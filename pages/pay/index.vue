05212ac3c3e96cce7b54545a2124864a0edc714c
Daazzer <Daazzer@github.com>
2020-12-02 18:16:56 +0800

    支付页的数据交互

--- pages/pay/index.vue
+++ pages/pay/index.vue
@@ -1,7 +1,17 @@
 <template>
   <view class="pay">
-    <view class="delivery-address">
-      <button class="delivery-address__btn" size="mini" plain>选择地址</button>
+    <view class="delivery-address" @click="selectAddress">
+      <view class="delivery-address__content" v-if="hasReceiverInfo">
+        <view class="delivery-receiver-address">{{ receiverInfo.address }}</view>
+        <view class="delivery-receiver-info">
+          <view class="delivery-receiver-info__receiver-name">{{ receiverInfo.name }}</view>
+          <view class="delivery-receiver-info__receiver-phone">{{ receiverInfo.phone }}</view>
+        </view>
+      </view>
+      <view
+        class="delivery-address__btn"
+        v-else
+      >选择地址</view>
     </view>
     <view class="checked-goods-list">
       <view class="checked-goods-list__title">已选商品</view>
@@ -15,7 +25,11 @@
         :goodsCount="checkedGoodsItem.goodsNum"
       />
     </view>
-    <GoodsCalcBar :totalPrice="totalPrice" :checkedNum="checkedGoodsNum">
+    <GoodsCalcBar
+      :totalPrice="totalPrice"
+      :checkedNum="checkedGoodsNum"
+      :disabledSettleBtn="!(hasReceiverInfo && checkedGoodsNum > 0)"
+    >
       <template>去支付</template>
     </GoodsCalcBar>
   </view>
@@ -33,7 +47,47 @@ export default {
   },
   data () {
     return {
-      checkedGoodsItems: []
+      checkedGoodsItems: [],
+      receiverInfo: {}
+    }
+  },
+  methods: {
+    selectAddress () {
+      uni.chooseAddress({
+        success: res => {
+          const {
+            provinceName,
+            cityName,
+            countyName,
+            detailInfo,
+            userName,
+            telNumber
+          } = res
+
+          const phoneReg = /(?:^(?:1\d{2})(\d{4})|^(?:0\d{2}-\d)(\d{4}))\d*/
+
+          // r1 匹配到的手机号码，r2 匹配到的座机号码
+          let [phone, r1, r2] = phoneReg.exec(telNumber)
+          let r = r1 ? r1 : r2
+
+          phone = phone.replace(r, '****')
+
+          this.receiverInfo = {
+            address: provinceName + cityName + countyName + detailInfo,
+            name: userName,
+            phone
+          }
+
+          let yougou = uni.getStorageSync('yougou')
+
+          if (!yougou) {
+            yougou = {}
+          }
+
+          yougou.receiverInfo = this.receiverInfo
+          uni.setStorageSync('yougou', yougou)
+        }
+      })
     }
   },
   computed: {
@@ -45,17 +99,26 @@ export default {
     checkedGoodsNum () {
       return this.checkedGoodsItems.length
     },
+    hasReceiverInfo () {
+      const keys = ['address', 'name', 'phone']
+
+      return keys.every(key => this.receiverInfo[key] !== '' && this.receiverInfo[key])
+    }
   },
   onShow () {
     const yougou = uni.getStorageSync('yougou')
 
     if (!yougou) {
+      this.checkedGoodsItems = []
+      this.receiverInfo = {}
       return
     }
 
     const goodsItems = yougou.cart
+    const receiverInfo = yougou.receiverInfo
 
     this.checkedGoodsItems = goodsItems ? goodsItems.filter(goodsItem => goodsItem.checked) : []
+    this.receiverInfo = receiverInfo ? receiverInfo : {}
   }
 }
 </script>
@@ -80,17 +143,42 @@ page {
   height: 187rpx;
   margin-bottom: 35rpx;
   &__btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 303rpx;
-    height: 89rpx;
     $color: #ea4350;
-    border: 2rpx solid $color;
-    border-radius: 44rpx;
-    color: $color;
-    font-size: 32rpx;
+    padding: 23rpx 88rpx;
+    font-size: 30rpx;
     font-weight: 600;
+    border: 2rpx solid $color;
+    border-radius: 45rpx;
+    color: $color;
+  }
+  &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    padding: 16rpx 17rpx 34rpx 30rpx;
+    flex: 1;
+    color: #434343;
+    .delivery-receiver {
+      &-address {
+        display: -webkit-box;
+        margin-bottom: 18rpx;
+        font-size: 30rpx;
+        font-weight: 600;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+      &-info {
+        display: flex;
+        font-size: 26rpx;
+        &__receiver-name {
+          margin-right: 40rpx;
+        }
+      }
+    }
   }
 }
 .checked-goods-list {
