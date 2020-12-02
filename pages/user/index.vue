e1869d80de82c442453b3cd7c69141d6256921c3
Daazzer <Daazzer@github.com>
2020-12-02 23:59:40 +0800

    用户页登录数据获取

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,17 +1,35 @@
 <template>
   <view class="user">
     <view class="user__info">
-      <view class="login-btn">去登陆</view>
+      <button
+        class="login-btn"
+        size="mini"
+        open-type="getUserInfo"
+        @getuserinfo="login"
+      >去登陆</button>
     </view>
     <view class="user__main">
       <view class="order-option">
         <view
-          v-for="(optionItem, index) in optionItems"
-          :key="optionItem.icon"
-          :class="{ 'order-option__item': true, 'active': index === activeOptionItem }"
+          v-for="(orderOptionItem, index) in orderOptionItems"
+          :key="orderOptionItem.icon"
+          :class="{ 'order-option__item': true, 'active': index === activeOrderOptionItem }"
         >
-          <text :class="'iconfont icon-' + optionItem.icon" />
-          <text class="order-option-text">{{ optionItem.text }}</text>
+          <text :class="'iconfont icon-' + orderOptionItem.icon" />
+          <text class="order-option-text">{{ orderOptionItem.text }}</text>
+        </view>
+      </view>
+      <view class="user-option">
+        <view
+          class="user-option__item"
+          v-for="userOptionItem in userOptionItems"
+          :key="userOptionItem.text"
+        >
+          <view class="user-option-content">
+            <text :class="'iconfont icon-' + userOptionItem.icon" />
+            <text>{{ userOptionItem.text }}</text>
+          </view>
+          <view class="user-option-subtext" v-if="userOptionItem.subText">{{ userOptionItem.subText }}</view>
         </view>
       </view>
     </view>
@@ -23,13 +41,28 @@ export default {
   name: 'User',
   data () {
     return {
-      activeOptionItem: 0,
-      optionItems: [
+      activeOrderOptionItem: 0,
+      orderOptionItems: [
         { text: '待付款', icon: 'icon-test' },
         { text: '待收货', icon: 'daishouhuo' },
         { text: '退货/退款', icon: 'tuihuo' },
         { text: '全部订单', icon: 'quanbudingdan01' }
+      ],
+      userOptionItems: [
+        { text: '联系客服', subText: '400-618-4000', icon: 'kefu' },
+        { text: '意见反馈', icon: 'fankuitianbao' },
+        { text: '当前版本', subText: 'v4.1.1', icon: 'icon-' }
       ]
+    }
+  },
+  methods: {
+    login (userInfo) {
+      uni.login({
+        success (code) {
+          console.log(code)
+          console.log(userInfo)
+        }
+      })
     }
   },
   onShow () {
@@ -63,7 +96,14 @@ page {
     }
   }
   &__main {
+    padding: 0 12rpx;
+    .order-option,
+    .user-option {
+      background-color: #fff;
+    }
     .order-option {
+      padding: 30rpx 0 14rpx;
+      margin-bottom: 20rpx;
       &, &__item {
         display: flex;
         justify-content: space-around;
@@ -80,6 +120,28 @@ page {
           margin-bottom: 16rpx;
           font-size: 60rpx;
         }
+      }
+    }
+    .user-option {
+      padding: 0 34rpx 0 28rpx;
+      min-height: 520rpx;
+      &__item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 30rpx 10rpx;
+        font-size: 26rpx;
+        border-bottom: 1px solid #c8c8c8;
+        color: #434343;
+        .iconfont {
+          margin-right: 28rpx;
+          font-size: 45rpx;
+        }
+      }
+      &-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
       }
     }
   }
